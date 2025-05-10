bc975ab0d9f9aa24155cd287c8a96c020e4f2f0b
Ainhoa <robledosa@esat-alumni.com>
2025-05-10 19:44:59 +0200

    Damage filter.

---
+++ Assets/Scripts/DamageFilter.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static PipeScript;
+public class DamageFilter : MonoBehaviour
+{
+  [SerializeField]
+  float maxLimit_ = 0.5f;
+  [SerializeField]
+  float speed_ = 1.0f;
+
+  private Image image_ = null;
+  private float alpha_ = 0.0f;
+
+  private bool isTintActive_ = false;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    image_ = GetComponent<Image>();
+    if (image_ == null) return;
+    OnRocketCollision += StartTint;
+  }
+
+  private void StartTint()
+  {
+    isTintActive_ = true;
+  }
+
+  private void TintScreen()
+  {
+    alpha_ += Time.deltaTime * speed_;
+    alpha_ = Mathf.Max(0.0f, alpha_);
+    
+    if(alpha_ >= maxLimit_)
+    {
+      speed_ *= -1.0f;
+    }
+    if (alpha_ == 0.0f)
+    {
+      speed_ *= -1.0f;
+      isTintActive_ = false;
+    }
+    image_.color = new Color(image_.color.r, image_.color.g, image_.color.b, alpha_);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (isTintActive_)
+      TintScreen();
+  }
+}
